896b08cc611a935a3e81976612ca4c61d023538c
MarcinLes7 <lmartino@wp.pl>
2023-06-04 10:10:36 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/jwdcolors1/Jwdcolors1Application.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwdcolors1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwdcolors1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwdcolors1Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wszib/jwdcolors1/Jwdcolors1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwdcolors1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwdcolors1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
